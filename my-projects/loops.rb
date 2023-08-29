fefcb742670d4ae126d8924e339cf68e8203551b
bad-juju999 <livelyjulian@gmail.com>
2023-08-29 16:26:47 -0500

    loops practice

---
+++ my-projects/loops.rb
@@ -0,0 +1,22 @@
+# i = 0
+# loop do
+#     puts i+= 1
+#     break if i == 10
+#     end
+
+#i = 0
+# while i != 10 do
+#     puts i += 1
+# end
+
+#looping question till answer is yes
+# puts "wanna play?"
+# ans = gets.chomp
+# until ans == "yes" do
+#     puts "wanna play now?"
+#     ans = gets.chomp
+# end
+
+for number in 1...5
+    puts "#{number} of enemise remaining"
+end
