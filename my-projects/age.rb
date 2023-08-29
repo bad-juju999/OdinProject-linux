759b0342b72f1d51ce2d16da7f25d57a4f8db58e
bad-juju999 <livelyjulian@gmail.com>
2023-08-29 13:56:24 -0500

    learn get.chomp

---
+++ my-projects/age.rb
@@ -0,0 +1,6 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you will be:" 
+puts age + 10
+puts "in 20 yrs:"
+puts age + 20
---
+++ my-projects/name.rb
@@ -0,0 +1,16 @@
+# puts "What is your name?"
+# user_name = gets.chomp
+# puts "Welcome, " + user_name +"!"
+
+# puts user_name
+
+# 10.times do
+#     puts user_name
+# end
+
+puts "what is your first name?"
+first_name = gets.chomp 
+puts "what is your last name?"
+last_name = gets.chomp
+puts first_name + " " + last_name
+
