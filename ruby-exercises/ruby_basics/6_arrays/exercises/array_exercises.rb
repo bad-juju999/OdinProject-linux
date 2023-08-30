0ea712bdfec7132ae90d3045127c48d80b7369e9
bad-juju999 <livelyjulian@gmail.com>
2023-08-30 12:35:01 -0500

    array exercises

--- ruby-exercises/ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby-exercises/ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,13 +1,16 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
+  nil_array.
 end
 
 def first_element(array)
   # return the first element of the array
+  array.first
 end
 
 def third_element(array)
   # return the third element of the array
+  array(2)
 end
 
 def last_three_elements(array)
@@ -16,11 +19,12 @@ end
 
 def add_element(array)
   # add an element (of any value) to the array
+  array.push (1)
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
+  array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
 end
@@ -34,6 +38,7 @@ end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  original + additional
 end
 
 def array_difference(original, comparison)
