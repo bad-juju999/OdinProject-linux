bc0a2b89e50f56e733d74004175dc50f5a2226c2
bad-juju999 <livelyjulian@gmail.com>
2023-09-10 19:43:39 -0500

    manipulating arrays

---
+++ my-projects/personal/array-practice.rb
@@ -0,0 +1,17 @@
+# array1 = [20,48,38,90,1,6,2,100]
+
+# sorted = array1.sort()
+# p sorted[0]
+# p sorted[sorted.length - 1]
+
+# p array1.min()
+# p array1.max()
+
+#finding a missing #.. in progress
+p array2 = [1,2,3,4,5,6,8,9,10]
+
+for x in 0...9
+    missing = false
+    if x == 
+    end
+end
