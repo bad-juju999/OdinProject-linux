bb3973a7ad95fdfcd116ad1eb183be65c18b9b0d
bad-juju999 <livelyjulian@gmail.com>
2023-09-19 21:29:34 -0500

    create more methods

--- my-projects/oop_exercises.rb
+++ my-projects/oop_exercises.rb
@@ -7,35 +7,36 @@ class MyCar
         @model = model
         @speed = 0
     end
-
     def year
         @year
     end
-
     def info
         puts "year: #{@year}, model: #{@model}, color:#{@color}"
     end
-
     def speed_up 
         puts @speed += 5
     end
-
     def slow 
         puts @speed += -5
     end
-
     def off 
         puts @speed = 0
     end
-
     def spray_paint(paint)
-        self.color = paint
+        color = paint
         puts "the color is now: #{color}"
+    end
+
+    def self.mpg_calc (miles, gallons)
+        puts miles/gallon
+    end
+
+    def to_s
+        puts "this is a #{@year}, #{@color}, #{@model}"
     end
 end
 
 jetta = MyCar.new(2012,"black","jetta")
-puts jetta.info 
-jetta.spray_paint("green")
-puts jetta.info 
+
+puts jetta
 
